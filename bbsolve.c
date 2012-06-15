8adfb531a7e6b4c1129778a91cc361ea34893c6c
David Reed <david@ktema.org>
2012-06-14 21:26:11 -0600

    Made bbsolve use getopt() for friendlier command-line use.

--- bbsolve.c
+++ bbsolve.c
@@ -18,6 +18,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <strings.h>
+#include <getopt.h>
+
+static struct option opts[] = {
+	{ "depth", required_argument, NULL, 'd' },
+	{ "pawn", required_argument, NULL, 'p' },
+	{ "token", required_argument, NULL, 't' },
+	{ "any-depth", no_argument, NULL, 'a' },
+	{ NULL, 0, NULL, 0 }
+};
 
 void iferror(bb_bool error, const char *errstr, ...) 
 {
@@ -32,25 +42,63 @@ void iferror(bb_bool error, const char *errstr, ...)
 	}
 }
 
+void usage()
+{
+	iferror(BB_TRUE, "bbsolve usage: bbsolve [--depth [-d] depth | --any-depth [-a]] --pawn [-p] pawn --token [-t] token BOARD\n");
+}
+
 int main (int argc, char **argv)
 {
 	int fd;
-	int depth;
+	int depth = -1;
 	off_t size;
 	char *b;
 	bb_board *board;
 	bb_pawn_state ps;
 	bb_token token;
-	bb_pawn pawn;
+	bb_pawn pawn = 0;
 	bb_array *solutions;
 	bb_fifo *fifo;
 	unsigned i;
-		
-	iferror (argc < 5, "bbsolve usage: bbsolve (depth) (pawn) (token) file\n");
+	char opt;
 	
-	fd = open(argv[4], O_RDONLY);	
-	iferror (fd == -1, "Could not open file %s\n", argv[2]);
+	while ((opt = getopt_long(argc, argv, "ad:p:t:", opts, NULL)) != -1) {
+		if (opt == 'd') {
+			depth = strtol(optarg, NULL, 0);
+		} else if (opt == 'a') {
+			depth = -1;
+		} else if (opt == 'p') {
+			if (strncasecmp(optarg, "red", 3) == 0) {
+				pawn = BB_PAWN_RED;
+			} else if (strncasecmp(optarg, "blue", 4) == 0) {
+				pawn = BB_PAWN_BLUE;
+			} else if (strncasecmp(optarg, "green", 5) == 0) {
+				pawn = BB_PAWN_GREEN;
+			} else if (strncasecmp(optarg, "yellow", 6) == 0) {
+				pawn = BB_PAWN_YELLOW;
+			} else if (strncasecmp(optarg, "silver", 6) == 0) {
+				pawn = BB_PAWN_SILVER;
+			} else {
+				usage();
+			}
+		} else if (opt == 't') {
+			token = strtol(optarg, NULL, 0);
+			iferror((token <= 0) || (token > BB_MAX_TOKEN), "Token value %d is out of range", token);
+		} else {
+			usage();
+		}
+	}
 	
+	argc -= optind;
+	argv += optind;
+	
+	if (argc >= 1) {
+		fd = open(argv[0], O_RDONLY);	
+		iferror (fd == -1, "Could not open file %s\n", argv[2]);
+	} else {
+		usage();
+	}
+
 	size = lseek(fd, 0, SEEK_END);
 	lseek(fd, 0, SEEK_SET);
 	
@@ -63,14 +111,6 @@ int main (int argc, char **argv)
 	bb_create_board_from_string(b, &board, ps);
 	free(b);
 	iferror(board == NULL, "Could not parse file.\n");
-	
-	depth = strtol(argv[1], NULL, 0);
-	
-	pawn = strtol(argv[2], NULL, 0);
-	iferror((pawn < BB_PAWN_RED) || (pawn > BB_PAWN_SILVER), "Pawn value %d is out of range", pawn);
-	
-	token = strtol(argv[3], NULL, 0);
-	iferror((token <= 0) || (token > BB_MAX_TOKEN), "Token value %d is out of range", token);
 	
 	fifo = bb_find_solutions(board, ps, pawn, token, depth);
 	solutions = bb_winnow_solutions(fifo);
